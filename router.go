fa86a58b3fcf9d6dab409505705b9c59bcf5d5fd
Jason <jason.chen@accuhit.net>
2022-05-03 14:11:25 +0800

    testing GET POT

--- router.go
+++ router.go
@@ -4,6 +4,7 @@ import (
 	v1 "api/api/v1"
 	_ "api/docs"
 	"api/service"
+	"net/http"
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
@@ -39,10 +40,45 @@ func Router() *gin.Engine {
 
 func Public(router *gin.RouterGroup) {
 	router.POST("/test", v1.Ping)
+	// router.GET('/login')
 }
 
 func Web(router *gin.RouterGroup) {
-	router.GET("/hello", func (c *gin.Context) {
-		c.JSON(200, "Hello - v1")
+	router.GET("/:name/:user_id", showUserInfo)
+	router.GET("/", showAction)
+	router.POST("/posts", showPosts)
+}
+
+func showUserInfo(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"name": c.Param("name"),
+		"userId": c.Param("user_id"),
 	})
+}
+
+func showAction(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"action": c.Query("method"),
+		"data": c.Query("data"),
+	})
+}
+/*
+raw json:
+{
+    "post_id": 12,
+    "title": "Test Text"
+}
+*/
+func showPosts(c *gin.Context) {
+	var response map[string]interface{}
+	err := c.Bind(&response)
+	if err != nil {
+		return;
+	}
+
+	c.JSON(200, gin.H{
+		"id": response["post_id"],
+	})
+	// or 
+	// c.JSON(200, gin.H(response))
 }
